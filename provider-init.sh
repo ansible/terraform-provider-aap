89178c638b2fabea46c07b3d8b9a0ee7c289d2c1
abikouo <abikouo@redhat.com>
2023-10-19 15:00:55 +0200

    Remove useless provider-init.sh

--- provider-init.sh
+++
@@ -1,6 +0,0 @@
-# Due to https://github.com/hashicorp/terraform-plugin-framework/issues/853
-
-go mod init terraform-provider-aap
-go mod edit -require github.com/hashicorp/terraform-plugin-framework@v1.3.5
-go mod edit -require github.com/hashicorp/terraform-plugin-go@v0.18.0
-go mod tidy 
