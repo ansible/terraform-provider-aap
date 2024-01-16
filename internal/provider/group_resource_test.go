8827815205f435b6066f3402508bf74e0f116df0
GomathiselviS <gomathiselvi@gmail.com>
2024-01-16 16:47:42 -0500

    Fix line len issue

--- internal/provider/group_resource_test.go
+++ internal/provider/group_resource_test.go
@@ -102,16 +102,17 @@ func TestGroupCreateRequestBody(t *testing.T) {
 			Name:        types.StringValue("group1"),
 			URL:         types.StringValue("/api/v2/groups/24/"),
 			Variables: jsontypes.NewNormalizedValue(
-				"{\"ansible_network_os\":\"ios\",\"ansible_connection\":\"network_cli\", \"ansible_ssh_user\":\"ansible\", \"ansible_ssh_pass\":\"ansible\"}",
+				"{\"ansible_network_os\":\"ios\",\"ansible_connection\":\"network_cli\",\"ansible_ssh_user\":\"ansible\",\"ansible_ssh_pass\":\"ansi\"}",
 			),
 			Description: types.StringValue("New Group"),
 		}
-		body := []byte(`{"name": "group1", "inventory": 5,
-		                "url": "/api/v2/groups/24/",
-						"description": "New Group",
-				        "variables":
-			"{\"ansible_network_os\":\"ios\", \"ansible_connection\":\"network_cli\", \"ansible_ssh_user\":\"ansible\", \"ansible_ssh_pass\":\"ansible\"}"}`,
-		)
+		body := []byte(`{
+    	"name": "group1",
+    	"inventory": 5,
+    	"url": "/api/v2/groups/24/",
+    	"description": "New Group",
+    	"variables": "{\"ansible_network_os\":\"ios\",\"ansible_connection\":\"network_cli\",\"ansible_ssh_user\":\"ansible\",\"ansible_ssh_pass\":\"ansi\"}"
+        }`)
 
 		result, diags := g.CreateRequestBody()
 		if diags.HasError() {
