77ee13fddd082437a72873ca0f699d88e905192f
GomathiselviS <gomathiselvi@gmail.com>
2024-02-22 10:19:16 -0500

    extract PR branch

--- internal/provider/host_resource.go
+++ internal/provider/host_resource.go
@@ -128,7 +128,7 @@ func (r *HostResource) Schema(_ context.Context, _ resource.SchemaRequest, resp 
 				PlanModifiers: []planmodifier.Bool{
 					boolplanmodifier.UseStateForUnknown(),
 				},
-				Description: "Denotes if the host is online and is available",
+				Description: "Denotes if the host is online and is available or not",
 			},
 			"groups": schema.SetAttribute{
 				ElementType: types.Int64Type,
