854fda863292d0b26e370aeee92b0fd0062fc1e3
Lisa Rashidi-Ranjbar <lranjbar@redhat.com>
2025-05-14 09:59:56 -0700

    Slight refactoring for and the creation of the job_template data source.

---
+++ internal/provider/job_template_data_source.go
@@ -0,0 +1,192 @@
+package provider
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"path"
+	"strings"
+
+	"github.com/ansible/terraform-provider-aap/internal/provider/customtypes"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+// Ensure the implementation satisfies the expected interfaces.
+var (
+	_ datasource.DataSource              = &JobTemplateDataSource{}
+	_ datasource.DataSourceWithConfigure = &JobTemplateDataSource{}
+)
+
+// NewJobTemplateDataSource is a helper function to simplify the provider implementation.
+func NewJobTemplateDataSource() datasource.DataSource {
+	return &JobTemplateDataSource{}
+}
+
+// JobTemplateDataSource is the data source implementation.
+type JobTemplateDataSource struct {
+	client *AAPClient
+}
+
+// Metadata returns the data source type name.
+func (d *JobTemplateDataSource) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_job_template"
+}
+
+// Schema defines the schema for the data source.
+func (d *JobTemplateDataSource) Schema(_ context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"id": schema.Int64Attribute{
+				Optional: true,
+				Description: "JobTemplate id",
+			},
+			"organization": schema.Int64Attribute{
+				Computed: true,
+				Description: "Identifier for the organization to which the JobTemplate belongs",
+			},
+			"organization_name": schema.StringAttribute{
+				Computed: true,
+				Optional: true,
+				Description: "The name for the organization to which the JobTemplate belongs",
+			},
+			"url": schema.StringAttribute{
+				Computed:    true,
+				Description: "Url of the JobTemplate",
+			},
+			"named_url": schema.StringAttribute{
+				Computed:    true,
+				Description: "The Named Url of the JobTemplate",
+			},
+			"name": schema.StringAttribute{
+				Computed:    true,
+				Optional: true,
+				Description: "Name of the JobTemplate",
+			},
+			"description": schema.StringAttribute{
+				Computed:    true,
+				Description: "Description of the JobTemplate",
+			},
+			"variables": schema.StringAttribute{
+				Computed:    true,
+				CustomType:  customtypes.AAPCustomStringType{},
+				Description: "Variables of the JobTemplate. Will be either JSON or YAML string depending on how the variables were entered into AAP.",
+			},
+		},
+		Description: `Get an existing JobTemplate.`,
+	}
+}
+
+// Read refreshes the Terraform state with the latest data.
+func (d *JobTemplateDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var state JobTemplateDataSourceModel
+	var diags diag.Diagnostics
+
+	// Read Terraform configuration data into the model
+	resp.Diagnostics.Append(req.Config.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	//Here is where we can get the "named" JobTemplate, which is "JobTemplate Name"++"Organization Name" to derive uniqueness
+	//we will take precedence if the Id is set to use that over the named_url attempt.
+
+	resourceURL := ""
+
+	if state.Id.String() != "<null>" {
+		resourceURL = path.Join(d.client.getApiEndpoint(), "job_templates", state.Id.String())
+	} else if state.Name.String() != "<null>" && state.OrganizationName.String() != "<null>"{
+		namedUrl := strings.Join([]string{state.Name.String()[1 : len(state.Name.String()) - 1], "++", state.OrganizationName.String()[1 : len(state.OrganizationName.String()) - 1]}, "")
+		resourceURL = path.Join(d.client.getApiEndpoint(), "job_templates", namedUrl)
+	} else { 
+		resp.Diagnostics.AddError("Minimal Data Not Supplied", "Require [id] or [name and organization_name]")
+		return
+	}
+
+	readResponseBody, diags := d.client.Get(resourceURL)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	diags = state.ParseHttpResponse(readResponseBody)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+	// Set state
+	// Save updated data into Terraform state
+	resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+// Configure adds the provider configured client to the data source.
+func (d *JobTemplateDataSource) Configure(_ context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	client, ok := req.ProviderData.(*AAPClient)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected *AAPClient, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	d.client = client
+}
+
+// JobTemplateDataSourceModel maps the data source schema data.
+type JobTemplateDataSourceModel struct {
+	Id           types.Int64                      `tfsdk:"id"`
+	Organization types.Int64                      `tfsdk:"organization"`
+	OrganizationName types.String                 `tfsdk:"organization_name"`
+	Url          types.String                     `tfsdk:"url"`
+	NamedUrl     types.String                     `tfsdk:"named_url"`
+	Name         types.String                     `tfsdk:"name"`
+	Description  types.String                     `tfsdk:"description"`
+	Variables    customtypes.AAPCustomStringValue `tfsdk:"variables"`
+}
+
+func (d *JobTemplateDataSourceModel) ParseHttpResponse(body []byte) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	// Unmarshal the JSON response
+	var apiJobTemplate JobTemplateAPIModel
+	err := json.Unmarshal(body, &apiJobTemplate)
+	if err != nil {
+		diags.AddError("Error parsing JSON response from AAP", err.Error())
+		return diags
+	}
+
+	// Map response to the JobTemplate datesource schema
+	d.Id = types.Int64Value(apiJobTemplate.Id)
+	d.Organization = types.Int64Value(apiJobTemplate.Organization)
+	d.OrganizationName = types.StringValue(apiJobTemplate.SummaryFields.Organization.Name)
+	d.Url = types.StringValue(apiJobTemplate.Url)
+	d.NamedUrl = types.StringValue(apiJobTemplate.Related.NamedUrl)
+	d.Name = ParseStringValue(apiJobTemplate.Name)
+	d.Description = ParseStringValue(apiJobTemplate.Description)
+	d.Variables = ParseAAPCustomStringValue(apiJobTemplate.Variables)
+
+	return diags
+}
+
+// JobTemplate AAP API model
+type JobTemplateAPIModel struct {
+	Id           int64  `json:"id,omitempty"`
+	Organization int64  `json:"organization"`
+	SummaryFields SummaryFieldsAPIModel `json:"summary_fields,omitempty"`
+	Url          string `json:"url,omitempty"`
+	Related          RelatedAPIModel `json:"related,omitempty"`
+	Name         string `json:"name"`
+	Description  string `json:"description,omitempty"`
+	Variables    string `json:"variables,omitempty"`
+}
