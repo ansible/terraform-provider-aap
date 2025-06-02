5955fdc8156c4c293470ce0d0bf8d681c5ad444a
matoval <mat24c@gmail.com>
2025-06-02 16:38:18 -0700

    Use utils func for tests

--- internal/provider/inventory_resource_test.go
+++ internal/provider/inventory_resource_test.go
@@ -171,7 +171,7 @@ func TestInventoryResourceParseHttpResponse(t *testing.T) {
 func TestAccInventoryResource(t *testing.T) {
 	var inventory InventoryAPIModel
 	randomName := acctest.RandStringFromCharSet(10, acctest.CharSetAlphaNum)
-	updatedOrganizationId := os.Getenv("AAP_TEST_ORGANIZATION_ID")
+	updatedOrgId := os.Getenv("AAP_TEST_ORGANIZATION_ID")
 	updatedName := "updated " + randomName
 	updatedDescription := "A test inventory"
 	updatedVariables := "{\"foo\": \"bar\"}"
@@ -187,46 +187,17 @@ func TestAccInventoryResource(t *testing.T) {
 			// Create and Read testing
 			{
 				Config: testAccInventoryResourceMinimal(randomName),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					testAccCheckInventoryResourceExists("aap_inventory.test", &inventory),
-					testAccCheckInventoryResourceValues(&inventory, randomName, "", ""),
-					resource.TestCheckResourceAttr("aap_inventory.test", "name", randomName),
-					resource.TestCheckResourceAttr("aap_inventory.test", "organization", "1"),
-					resource.TestCheckResourceAttr("aap_inventory.test", "organization_name", "Default"),
-					resource.TestCheckResourceAttrSet("aap_inventory.test", "named_url"),
-					resource.TestCheckResourceAttrSet("aap_inventory.test", "id"),
-					resource.TestCheckResourceAttrSet("aap_inventory.test", "url"),
-				),
+				Check:  checkBasicInventoryAttributes(t, resourceNameInventory, inventory, randomName, "1", "Default", "", ""),
 			},
 			// Update with new org and Read testing
 			{
-				Config: testAccInventoryResourceWithOrg(updatedName, updatedOrganizationId),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					testAccCheckInventoryResourceExists("aap_inventory.test", &inventory),
-					testAccCheckInventoryResourceValues(&inventory, updatedName, updatedDescription, updatedVariables),
-					resource.TestCheckResourceAttr("aap_inventory.test", "name", updatedName),
-					resource.TestCheckResourceAttr("aap_inventory.test", "organization", updatedOrganizationId),
-					resource.TestCheckResourceAttr("aap_inventory.test", "description", updatedDescription),
-					resource.TestCheckResourceAttr("aap_inventory.test", "variables", updatedVariables),
-					resource.TestCheckResourceAttr("aap_inventory.test", "named_url", fmt.Sprintf("/api/controller/v2/inventories/%s++%s/", updatedName, "test")),
-					resource.TestCheckResourceAttrSet("aap_inventory.test", "id"),
-					resource.TestCheckResourceAttrSet("aap_inventory.test", "url"),
-				),
+				Config: testAccInventoryResourceWithOrg(updatedName, updatedOrgId),
+				Check:  checkBasicInventoryAttributes(t, resourceNameInventory, inventory, updatedName, updatedOrgId, "Default2", updatedDescription, updatedVariables),
 			},
 			// Update without new org and Read testing
 			{
 				Config: testAccInventoryResourceComplete(updatedName),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					testAccCheckInventoryResourceExists("aap_inventory.test", &inventory),
-					testAccCheckInventoryResourceValues(&inventory, updatedName, updatedDescription, updatedVariables),
-					resource.TestCheckResourceAttr("aap_inventory.test", "name", updatedName),
-					resource.TestCheckResourceAttr("aap_inventory.test", "organization", "1"),
-					resource.TestCheckResourceAttr("aap_inventory.test", "description", updatedDescription),
-					resource.TestCheckResourceAttr("aap_inventory.test", "variables", updatedVariables),
-					resource.TestCheckResourceAttr("aap_inventory.test", "named_url", fmt.Sprintf("/api/controller/v2/inventories/%s++%s/", updatedName, "Default")),
-					resource.TestCheckResourceAttrSet("aap_inventory.test", "id"),
-					resource.TestCheckResourceAttrSet("aap_inventory.test", "url"),
-				),
+				Check:  checkBasicInventoryAttributes(t, resourceNameInventory, inventory, updatedName, "1", "Default", updatedDescription, updatedVariables),
 			},
 		},
 		CheckDestroy: testAccCheckInventoryResourceDestroy,
