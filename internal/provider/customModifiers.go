5684f0e3df96c11341a4d20c653e21dbb6a8061f
matoval <mat24c@gmail.com>
2025-06-05 15:10:34 -0700

    Lint

--- internal/provider/customModifiers.go
+++ internal/provider/customModifiers.go
@@ -8,31 +8,31 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 )
 
-type warnIfDefaultUsedInt64Modifier struct{
-  attributeName string
-  releaseVersion string
+type warnIfDefaultUsedInt64Modifier struct {
+	attributeName  string
+	releaseVersion string
 }
 
 func (m warnIfDefaultUsedInt64Modifier) Description(ctx context.Context) string {
-  return fmt.Sprintf("Warns if the default value for '%s' is used during plan.", m.attributeName)
+	return fmt.Sprintf("Warns if the default value for '%s' is used during plan.", m.attributeName)
 }
 
 func (m warnIfDefaultUsedInt64Modifier) MarkdownDescription(ctx context.Context) string {
-  return m.Description(ctx)
+	return m.Description(ctx)
 }
 
 func (m warnIfDefaultUsedInt64Modifier) PlanModifyInt64(ctx context.Context, req planmodifier.Int64Request, resp *planmodifier.Int64Response) {
-  if req.ConfigValue.IsNull() || req.ConfigValue.IsUnknown() {
-    resp.Diagnostics.Append(diag.NewWarningDiagnostic(
-      "Default vaule Used",
-      fmt.Sprintf("The default value for %s is being used. %s will be a required value in release %s", m.attributeName, m.attributeName, m.releaseVersion),
-    ))
-  }
+	if req.ConfigValue.IsNull() || req.ConfigValue.IsUnknown() {
+		resp.Diagnostics.Append(diag.NewWarningDiagnostic(
+			"Default vaule Used",
+			fmt.Sprintf("The default value for %s is being used. %s will be a required value in release %s", m.attributeName, m.attributeName, m.releaseVersion),
+		))
+	}
 }
 
 func WarnIfDefaultInt64Used(attributeName string, releaseVersion string) planmodifier.Int64 {
-  return warnIfDefaultUsedInt64Modifier{
-    attributeName: attributeName,
-    releaseVersion: releaseVersion,
-  }
+	return warnIfDefaultUsedInt64Modifier{
+		attributeName:  attributeName,
+		releaseVersion: releaseVersion,
+	}
 }
