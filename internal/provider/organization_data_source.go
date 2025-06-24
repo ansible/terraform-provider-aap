69a0b618c1eda8134b9864c0f171f669a328c8ec
Andrew Potozniak <potozniak@redhat.com>
2025-06-24 14:30:50 -0400

    Updates for refactorings

--- internal/provider/organization_data_source.go
+++ internal/provider/organization_data_source.go
@@ -2,96 +2,42 @@ package provider
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"path"
 
-	"github.com/ansible/terraform-provider-aap/internal/provider/customtypes"
-	"github.com/hashicorp/terraform-plugin-framework-validators/datasourcevalidator"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
-	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
-	tfpath "github.com/hashicorp/terraform-plugin-framework/path"
-	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 // Organization AAP API model
 type OrganizationAPIModel struct {
-	Id            int64                 `json:"id,omitempty"`
-	SummaryFields SummaryFieldsAPIModel `json:"summary_fields,omitempty"`
-	Url           string                `json:"url,omitempty"`
-	Related       RelatedAPIModel       `json:"related,omitempty"`
-	Name          string                `json:"name"`
-	Description   string                `json:"description,omitempty"`
-	Variables     string                `json:"variables,omitempty"`
+	BaseDetailAPIModel
 }
 
 // organizationDataSourceModel maps the data source schema data.
 type OrganizationDataSourceModel struct {
-	Id          types.Int64                      `tfsdk:"id"`
-	Url         types.String                     `tfsdk:"url"`
-	NamedUrl    types.String                     `tfsdk:"named_url"`
-	Name        types.String                     `tfsdk:"name"`
-	Description types.String                     `tfsdk:"description"`
-	Variables   customtypes.AAPCustomStringValue `tfsdk:"variables"`
+	BaseDetailDataSourceModel
 }
 
 // OrganizationDataSource is the data source implementation.
 type OrganizationDataSource struct {
-	client ProviderHTTPClient
+	BaseDataSource
 }
-
-// Ensure the implementation satisfies the expected interfaces.
-var (
-	_ datasource.DataSource                     = &OrganizationDataSource{}
-	_ datasource.DataSourceWithConfigure        = &OrganizationDataSource{}
-	_ datasource.DataSourceWithConfigValidators = &OrganizationDataSource{}
-	_ datasource.DataSourceWithValidateConfig   = &OrganizationDataSource{}
-)
 
 // NewOrganizationDataSource is a helper function to simplify the provider implementation.
 func NewOrganizationDataSource() datasource.DataSource {
-	return &OrganizationDataSource{}
-}
-
-// Metadata returns the data source type name.
-func (d *OrganizationDataSource) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
-	resp.TypeName = req.ProviderTypeName + "_organization"
+	return &OrganizationDataSource{
+		BaseDataSource: *NewBaseDataSource(nil, StringDescriptions{
+			MetadataEntitySlug:    "organization",
+			DescriptiveEntityName: "Organization",
+			ApiEntitySlug:         "organizations",
+		}),
+	}
 }
 
 // Schema defines the schema for the data source.
 func (d *OrganizationDataSource) Schema(_ context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
-	resp.Schema = schema.Schema{
-		Attributes: map[string]schema.Attribute{
-			"id": schema.Int64Attribute{
-				Optional:    true,
-				Description: "Organization id",
-			},
-			"name": schema.StringAttribute{
-				Computed:    true,
-				Optional:    true,
-				Description: "Name of the organization",
-			},
-			"url": schema.StringAttribute{
-				Computed:    true,
-				Description: "Url of the organization",
-			},
-			"named_url": schema.StringAttribute{
-				Computed:    true,
-				Description: "The Named Url of the organization",
-			},
-			"description": schema.StringAttribute{
-				Computed:    true,
-				Description: "Description of the organization",
-			},
-			"variables": schema.StringAttribute{
-				Computed:    true,
-				CustomType:  customtypes.AAPCustomStringType{},
-				Description: "Variables of the organization. Will be either JSON or YAML string depending on how the variables were entered into AAP.",
-			},
-		},
-		Description: `Get an existing organization.`,
-	}
+	resp.Schema = GetBaseSchema(d.DescriptiveEntityName, d.MetadataEntitySlug)
 }
 
 // Read refreshes the Terraform state with the latest data.
@@ -106,7 +52,8 @@ func (d *OrganizationDataSource) Read(ctx context.Context, req datasource.ReadRe
 	}
 
 	uri := path.Join(d.client.getApiEndpoint(), "organizations")
-	resourceURL, err := ReturnAAPOrganizationNamedURL(state.Id, state.Name, uri)
+	resourceURL, err := ReturnAAPNamedURLWithoutOrganization(state.Id, state.Name, uri)
+
 	if err != nil {
 		resp.Diagnostics.AddError("Minimal Data Not Supplied", "Expected either [id] or [name]")
 		return
@@ -114,6 +61,7 @@ func (d *OrganizationDataSource) Read(ctx context.Context, req datasource.ReadRe
 
 	readResponseBody, diags := d.client.Get(resourceURL)
 	resp.Diagnostics.Append(diags...)
+
 	if resp.Diagnostics.HasError() {
 		return
 	}
@@ -152,76 +100,11 @@ func (d *OrganizationDataSource) Configure(_ context.Context, req datasource.Con
 
 func (d *OrganizationDataSource) ConfigValidators(_ context.Context) []datasource.ConfigValidator {
 	// You have at least an id or a name + organization_name pair
-	return []datasource.ConfigValidator{
-		datasourcevalidator.AtLeastOneOf(
-			tfpath.MatchRoot("id"),
-			tfpath.MatchRoot("name"),
-		),
-	}
+	return GetIdOrNameDataSourceConfigValidator()
 }
 
 func (d *OrganizationDataSource) ValidateConfig(ctx context.Context, req datasource.ValidateConfigRequest, resp *datasource.ValidateConfigResponse) {
 	var data OrganizationDataSourceModel
-
 	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
-
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	if IsValueProvided(data.Id) && IsValueProvided(data.Name) {
-		resp.Diagnostics.AddAttributeError(
-			tfpath.Root("id"),
-			"Attribute Precedence",
-			"When both [id] and [name] are defined for aap_organization, id takes precedence.",
-		)
-		resp.Diagnostics.AddAttributeWarning(
-			tfpath.Root("name"),
-			"Attribute Precedence",
-			"When both [id] and [name] are defined for aap_organization, id takes precedence.",
-		)
-	}
-
-	if IsValueProvided(data.Id) {
-		return
-	}
-
-	if IsValueProvided(data.Name) {
-		return
-	}
-
-	if !IsValueProvided(data.Id) && !IsValueProvided(data.Name) {
-		resp.Diagnostics.AddAttributeWarning(
-			tfpath.Root("id"),
-			"Missing Attribute Configuration",
-			"Expected either [id] or [name]",
-		)
-		resp.Diagnostics.AddAttributeWarning(
-			tfpath.Root("name"),
-			"Missing Attribute Configuration",
-			"Expected either [id] or [name]",
-		)
-	}
-}
-
-func (dm *OrganizationDataSourceModel) ParseHttpResponse(body []byte) diag.Diagnostics {
-	var diags diag.Diagnostics
-
-	// Unmarshal the JSON response
-	var apiOrganization OrganizationAPIModel
-	err := json.Unmarshal(body, &apiOrganization)
-	if err != nil {
-		diags.AddError("Error parsing JSON response from AAP", err.Error())
-		return diags
-	}
-
-	// Map response to the organization datesource schema
-	dm.Id = types.Int64Value(apiOrganization.Id)
-	dm.Name = ParseStringValue(apiOrganization.Name)
-	dm.Url = types.StringValue(apiOrganization.Url)
-	dm.NamedUrl = types.StringValue(apiOrganization.Related.NamedUrl)
-	dm.Description = ParseStringValue(apiOrganization.Description)
-	dm.Variables = ParseAAPCustomStringValue(apiOrganization.Variables)
-
-	return diags
+	AppendIdOrNameConfigurationValidationResults(resp, data.Id, data.Name)
 }
