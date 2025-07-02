e6b4a9831ad5975acd49639eed6e6e6c25ce1668
Lisa Rashidi-Ranjbar <lranjbar@redhat.com>
2025-07-02 12:58:32 -0700

    Remove unused code

--- internal/provider/aap_named_url.go
+++ internal/provider/aap_named_url.go
@@ -7,36 +7,9 @@ import (
 	"strconv"
 )
 
-// Here for reference will be removed
-// type BaseDetailAPIModel struct {
-// 	Id            int64                 `json:"id"`
-// 	Name          string                `json:"name,omitempty"`
-// 	Description   string                `json:"description,omitempty"`
-// 	URL           string                `json:"url"`
-// 	Related       RelatedAPIModel       `json:"related"`
-// 	SummaryFields SummaryFieldsAPIModel `json:"summary_fields"`
-// 	Variables     string                `json:"variables,omitempty"`
-// }
-//
-// type BaseDetailAPIModelWithOrg struct {
-// 	BaseDetailAPIModel
-// 	Organization int64 `json:"organization"`
-// }
-
 // ---------------------------------------------------------------------------
 // BaseDetailAPIModel
 // ---------------------------------------------------------------------------
-
-// Id only CreateNameURL equivalent would need the refactor of our API models to be complete.
-// This helps show why Id and URL need to be pulled out.
-
-// func (o *BaseDetailAPIModel) CreateNamedURL(uri string) (string, error) {
-// 	if o.Id != 0 {
-// 		return path.Join(uri, strconv.FormatInt(o.Id, 10)), nil
-// 	}
-
-// 	return "", errors.New("invalid lookup parameters: id required")
-// }
 
 func (o *BaseDetailAPIModel) CreateNamedURL(uri string) (string, error) {
 	if o.Id != 0 {
