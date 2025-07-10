6cac4f10c776a0399f499cf7fdc819c199c5b36c
Andrea Restle-Lay <arestlel@redhat.com>
2025-07-10 10:28:56 -0400

    change id to name

--- env.sh
+++ env.sh
@@ -8,4 +8,4 @@ export TF_ACC=1
 
 # TODO: Makefile to build these out automatically.in aap-dev to build these. This was de-prioritized. see https://issues.redhat.com/browse/AAP-44341
 export AAP_TEST_ORGANIZATION_NAME=Non-Default
-export AAP_TEST_WORKFLOW_JOB_TEMPLATE_ID="Demo Workflow Job Template" # needs inventory prompt on launch and default or nd demo inventory and a step with default job template.
+export AAP_TEST_WORKFLOW_JOB_TEMPLATE_NAME="Demo Workflow Job Template" # needs inventory prompt on launch and default or nd demo inventory and a step with default job template.
