079eb85569d44acdaa3c4d45fbc234a6f4482d82
Andrea Restle-Lay <arestlel@redhat.com>
2025-07-15 13:02:58 -0400

    override ConfigValidator for organization_data_source

--- internal/provider/organization_data_source.go
+++ internal/provider/organization_data_source.go
@@ -1,7 +1,11 @@
 package provider
 
 import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework-validators/datasourcevalidator"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 )
 
 // Organization AAP API model
@@ -17,6 +21,18 @@ type OrganizationDataSourceModel struct {
 // OrganizationDataSource is the data source implementation.
 type OrganizationDataSource struct {
 	BaseDataSource
+}
+
+// ConfigValidators returns a list of validators for the data source configuration.
+// Overrides BaseDataSource to allow both id and name as lookup options.
+func (d *OrganizationDataSource) ConfigValidators(_ context.Context) []datasource.ConfigValidator {
+	return []datasource.ConfigValidator{
+		datasourcevalidator.Any(
+			datasourcevalidator.AtLeastOneOf(
+				path.MatchRoot("id"),
+				path.MatchRoot("name")),
+		),
+	}
 }
 
 // NewOrganizationDataSource is a helper function to simplify the provider implementation.
