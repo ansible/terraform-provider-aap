56643faa9b974c3e970f8ef42fd1a1a7447e3285
Dave Mulford <243049+davemulford@users.noreply.github.com>
2025-11-05 07:53:22 -0500

    [AAP-54793] Add event stream datasource docs (#164)
    Adds documentation for the new EDA Event Stream Datasource.
    
    Co-authored-by: Daniel Quackenbush <25692880+danquack@users.noreply.github.com>
    Co-authored-by: Dan Leehr <dleehr@users.noreply.github.com>

--- internal/provider/base_eda_datasource.go
+++ internal/provider/base_eda_datasource.go
@@ -38,13 +38,16 @@ func (d *BaseEdaDataSource) Metadata(_ context.Context, req datasource.MetadataR
 func (d *BaseEdaDataSource) GetBaseAttributes() map[string]schema.Attribute {
 	return map[string]schema.Attribute{
 		"id": schema.Int64Attribute{
-			Computed: true,
+			Computed:    true,
+			Description: fmt.Sprintf("%s id", d.DescriptiveEntityName),
 		},
 		"name": schema.StringAttribute{
-			Required: true,
+			Required:    true,
+			Description: fmt.Sprintf("Name of the %s", d.DescriptiveEntityName),
 		},
 		"url": schema.StringAttribute{
-			Computed: true,
+			Computed:    true,
+			Description: fmt.Sprintf("URL of the %s", d.DescriptiveEntityName),
 		},
 	}
 }
@@ -53,7 +56,7 @@ func (d *BaseEdaDataSource) GetBaseAttributes() map[string]schema.Attribute {
 func (d *BaseEdaDataSource) Schema(_ context.Context, _ datasource.SchemaRequest, resp *datasource.SchemaResponse) {
 	resp.Schema = schema.Schema{
 		Attributes:  d.GetBaseAttributes(),
-		Description: fmt.Sprintf("Creates a %s.", d.DescriptiveEntityName),
+		Description: fmt.Sprintf("Gets an existing %s.", d.DescriptiveEntityName),
 	}
 }
 
